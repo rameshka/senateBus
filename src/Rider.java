8fc9df865c5c9dfde265ed65260a86da6aee6c1e
piyumi <rameshkafox@gmail.com>
2018-02-10 20:07:47 +0530

    updating rider

--- src/Rider.java
+++ src/Rider.java
@@ -8,23 +8,29 @@ public class Rider extends Thread {
 
     public void run() {
         try {
-            System.out.println("thread name" + getName());
+            //Multiplex enables only maximum of 50 riders are able to wait
             sharedData.getMultiplex().acquire();
-            sharedData.getMutex().acquire();    //similar to a lock
-            sharedData.setRiders();
+
+            //rider entering critical region to increment rider count
+            //A rider arriving after bus arrival can not increment the rider count
+            System.out.println("Passenger: "+ getName() + " waiting till bus arrives");
+            sharedData.getMutex().acquire();
+            sharedData.incrementRiders();
             sharedData.getMutex().release();
 
-            System.out.println("wait Till Bus.... " + getName());
-            sharedData.getBusArrival().acquire();
+            sharedData.getBusArrival().acquire(); //first rider can continue once bus has arrived
             sharedData.getMultiplex().release();
 
             boardBus();
 
+            //decrementing the waiting rider are not synchronize as only one rider can board at a time (no concurrent issues)
             sharedData.decrementRiders();
 
             if (sharedData.getRiders() == 0) {
+                //last rider informs the bus, that all eligible waiting passengers are boarded
                 sharedData.getAllAboard().release();
             } else {
+                //each rider informs one another eligible rider that bus has arrived
                 sharedData.getBusArrival().release();
             }
 
@@ -35,6 +41,6 @@ public class Rider extends Thread {
     }
 
     private void boardBus() {
-        System.out.println("boarding to the bus "+ getName());
+        System.out.println("Passenger: "+ getName() + " boarding to the bus");
     }
 }
